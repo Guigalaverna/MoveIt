1dd71fb1184b111fc0421d76b80c20c8da200f0d
Guigalaverna <guigalaverna@gmail.com>
2021-02-27 22:44:46 -0300

    :rocket: Aula 03 - Countdown completado

--- MoveIt/frontend/src/components/Countdown/index.tsx
+++ MoveIt/frontend/src/components/Countdown/index.tsx
@@ -1,20 +1,57 @@
+import { useEffect, useState } from 'react'
 import styles from './Countdown.module.css'
 
+let countdownTimeout: NodeJS.Timeout
+
 export default function Countdown() {
+  const [time, setTime] = useState(25 * 60)
+
+  const [isActive, setIsActive] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('') 
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('') 
+
+  function startCountdown() {
+    setTime(25 * 60)
+    setIsActive(true)
+  }
+
+  function stopCountdown() {
+    setIsActive(false)
+    clearTimeout(countdownTimeout)
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1)
+      }, 1000) 
+    }
+  }, [isActive, time])
+
   return (
     <>
       <div className={styles.countdownContainer}>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </div>
-      <button type='button' className={styles.startCountdownButton}>Iniciar um ciclo</button>
+
+      { isActive ? (
+        <button type='button' onClick={stopCountdown} className={styles.pauseCountdownButton}>Abandonar ciclo</button>
+      ) : (
+        <button type='button' onClick={startCountdown} className={styles.startCountdownButton}>Iniciar um ciclo</button>
+      )}
+
     </>
   )
 }
